78b4e36813c2f2e6ec531de32ce8b9865ddce099
Ghazal <37179631+GhazalKazemi@users.noreply.github.com>
2024-05-17 16:44:06 +1000

    Create mapping between entities

--- spring6playground/spring-web-app/spring6webapp/src/main/java/com/ghazal/spring6webapp/entity/Book.java
+++ spring6playground/spring-web-app/spring6webapp/src/main/java/com/ghazal/spring6webapp/entity/Book.java
@@ -1,9 +1,8 @@
 package com.ghazal.spring6webapp.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.util.Set;
 
 @Entity
 public class Book {
@@ -12,6 +11,10 @@ public class Book {
     private Long id;
     private String title;
     private String isbn;
+    @ManyToMany
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns =@JoinColumn(name =  "author_id"))
+    private Set<Author> authors;
 
     public String getTitle() {
         return title;
@@ -27,5 +30,13 @@ public class Book {
 
     public void setIsbn(String isbn) {
         this.isbn = isbn;
+    }
+
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
     }
 }
