bbe975677891f78baa72c760c9981357e68b5afc
Ghazal <37179631+GhazalKazemi@users.noreply.github.com>
2024-05-17 17:21:18 +1000

    Implement equals, hashcode and toString

--- spring6playground/spring-web-app/spring6webapp/src/main/java/com/ghazal/spring6webapp/entity/Author.java
+++ spring6playground/spring-web-app/spring6webapp/src/main/java/com/ghazal/spring6webapp/entity/Author.java
@@ -2,6 +2,7 @@ package com.ghazal.spring6webapp.entity;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -36,5 +37,29 @@ public class Author {
 
     public void setBooks(Set<Book> books) {
         this.books = books;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Author author = (Author) o;
+        return Objects.equals(id, author.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", books=" + books +
+                '}';
     }
 }
--- spring6playground/spring-web-app/spring6webapp/src/main/java/com/ghazal/spring6webapp/entity/Book.java
+++ spring6playground/spring-web-app/spring6webapp/src/main/java/com/ghazal/spring6webapp/entity/Book.java
@@ -2,6 +2,7 @@ package com.ghazal.spring6webapp.entity;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -38,5 +39,29 @@ public class Book {
 
     public void setAuthors(Set<Author> authors) {
         this.authors = authors;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+        return Objects.equals(id, book.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", authors=" + authors +
+                '}';
     }
 }
